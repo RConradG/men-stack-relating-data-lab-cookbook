4f55bc9d7eca5765335e73339b114c0edf210a31
RConradGe <rcgadduang@gmail.com>
2025-03-13 22:14:50 -0700

    added router to render pantr«

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
+
+// GET /users/:userID/foods <- already here
+
+router.get('/', async (req, res) => {
+  // res.send("this is the pantry that shows foods")
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    res.render('foods/index.ejs', {
+      foods: currentUser.foods
+    });
+  } catch(error) {
+    console.log(error);
+    res.redirect('/');
+  };
+});
+
 
 module.exports = router;
